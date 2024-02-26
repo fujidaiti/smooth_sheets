0b8afc41759c62d8c2aaefbc9781616ea5015249
fujidaiti <fujidaiti@gmail.com>
2024-02-26 23:52:59 +0900

    Document sized_content_sheet

--- package/lib/src/foundation/sized_content_sheet.dart
+++ package/lib/src/foundation/sized_content_sheet.dart
@@ -5,7 +5,9 @@ import 'package:smooth_sheets/src/foundation/sheet_controller.dart';
 import 'package:smooth_sheets/src/foundation/sheet_extent.dart';
 import 'package:smooth_sheets/src/foundation/sheet_physics.dart';
 
+/// [SheetExtent] for a [SizedContentSheet].
 abstract class SizedContentSheetExtent extends SheetExtent {
+  /// Creates a [SheetExtent] for [SizedContentSheet]s.
   SizedContentSheetExtent({
     required super.context,
     required super.physics,
@@ -14,10 +16,15 @@ abstract class SizedContentSheetExtent extends SheetExtent {
     required this.initialExtent,
   });
 
+  /// {@template SizedContentSheetExtent.initialExtent}
+  /// The initial extent of the sheet when it is first shown.
+  /// {@endtemplate}
   final Extent initialExtent;
 }
 
+/// Factory of [SizedContentSheetExtent].
 abstract class SizedContentSheetExtentFactory extends SheetExtentFactory {
+  /// Creates a factory of [SizedContentSheetExtent].
   const SizedContentSheetExtentFactory({
     required this.initialExtent,
     required this.minExtent,
@@ -25,14 +32,22 @@ abstract class SizedContentSheetExtentFactory extends SheetExtentFactory {
     required this.physics,
   });
 
+  /// {@macro SizedContentSheetExtent.initialExtent}
   final Extent initialExtent;
+
+  /// {@macro SheetExtent.minExtent}
   final Extent minExtent;
+
+  /// {@macro SheetExtent.maxExtent}
   final Extent maxExtent;
+
+  /// {@macro SheetExtent.physics}
   final SheetPhysics physics;
 }
 
-/// A base class for sheets that have a single content.
+/// A base class for sheets that have an explicit initial extent.
 abstract class SizedContentSheet extends StatefulWidget {
+  /// Creates a sheet with an explicit initial extent.
   const SizedContentSheet({
     super.key,
     this.keyboardDismissBehavior,
@@ -46,23 +61,42 @@ abstract class SizedContentSheet extends StatefulWidget {
     required this.child,
   });
 
+  /// The strategy to dismiss the on-screen keyboard when the sheet is dragged.
   final SheetKeyboardDismissBehavior? keyboardDismissBehavior;
+
+  /// {@macro SizedContentSheetExtent.initialExtent}
   final Extent initialExtent;
+
+  /// {@macro SheetExtent.minExtent}
   final Extent minExtent;
+
+  /// {@macro SheetExtent.maxExtent}
   final Extent maxExtent;
+
+  /// {@macro SheetExtent.physics}
   final SheetPhysics physics;
+
+  /// An object that can be used to control and observe the sheet height.
   final SheetController? controller;
+
+  /// The content of the sheet.
   final Widget child;
 
   @override
   SizedContentSheetState createState();
 }
 
+/// State for a [SizedContentSheet].
 abstract class SizedContentSheetState<T extends SizedContentSheet>
     extends State<T> {
-  late SheetExtentFactory _factory;
+  /// The factory of [SheetExtent] which drives this sheet.
+  ///
+  /// This is first created in [initState] and updated in [didUpdateWidget]
+  /// when the [SizedContentSheet] properties change.
   SheetExtentFactory get factory => _factory;
+  late SheetExtentFactory _factory;
 
+  /// Creates the factory of [SheetExtent] for this sheet.
   SheetExtentFactory createExtentFactory();
 
   @override
