06e4d6e47f1d9ccdcca41f00642a5b8323696700
fujidaiti <fujidaiti@gmail.com>
2025-05-18 21:08:57 +0900

    add regression test

--- test/cupertino_test.dart
+++ test/cupertino_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:smooth_sheets/src/activity.dart';
 import 'package:smooth_sheets/src/cupertino.dart';
 import 'package:smooth_sheets/src/gesture_proxy.dart';
@@ -387,119 +388,92 @@ void main() {
       },
     );
 
+    // https://github.com/fujidaiti/smooth_sheets/issues/355
     testWidgets(
-      '#355: Pop after closing a sheet from a pushed page causes exception (Navigator)',
+      'Crashes when popping a route just below the modal sheet',
       (tester) async {
-        const homeToDetailButtonKey = ValueKey('home_to_detail_button');
-        const detailShowSheetButtonKey = ValueKey('detail_show_sheet_button');
-        const detailGoHomeButtonKey = ValueKey('detail_go_home_button');
-        const sheetCloseButtonKey = ValueKey('sheet_close_button');
+        Widget buildSheet(BuildContext context) {
+          return Sheet(
+            key: const ValueKey('sheet'),
+            child: Container(
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              height: 300,
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                child: const Text('Close Sheet'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+          );
+        }
 
-        await tester.pumpWidget(
-          CupertinoApp(
-            home: Builder(builder: (homeContext) {
-              // Home Page
-              return CupertinoPageScaffold(
-                child: Center(
-                  child: CupertinoButton(
-                    key: homeToDetailButtonKey,
-                    child: const Text('Go to Detail'),
+        Widget buildDetailPage(BuildContext context) {
+          return CupertinoPageScaffold(
+            key: const ValueKey('detail-page'),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CupertinoButton(
+                    child: const Text('Show Modal Sheet'),
                     onPressed: () {
-                      Navigator.of(homeContext).push(
-                        CupertinoPageRoute(builder: (detailContext) {
-                          // Detail Page
-                          return CupertinoPageScaffold(
-                            navigationBar: const CupertinoNavigationBar(
-                                middle: Text('Detail Page')),
-                            child: Center(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  CupertinoButton(
-                                    key: detailShowSheetButtonKey,
-                                    child: const Text('Show Modal Sheet'),
-                                    onPressed: () {
-                                      Navigator.of(detailContext).push(
-                                        CupertinoModalSheetRoute(
-                                          builder: (sheetContext) {
-                                            // Sheet Content
-                                            return Sheet(
-                                              child: Container(
-                                                color: CupertinoColors
-                                                    .systemBackground
-                                                    .resolveFrom(sheetContext),
-                                                height: 300,
-                                                alignment: Alignment.center,
-                                                child: CupertinoButton(
-                                                  key: sheetCloseButtonKey,
-                                                  child:
-                                                      const Text('Close Sheet'),
-                                                  onPressed: () =>
-                                                      Navigator.of(sheetContext)
-                                                          .pop(),
-                                                ),
-                                              ),
-                                            );
-                                          },
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  CupertinoButton(
-                                    key: detailGoHomeButtonKey,
-                                    child: const Text('Go to Home'),
-                                    onPressed: () => Navigator.of(detailContext)
-                                        .pop(), // This pops DetailPage
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        }),
+                      Navigator.of(context).push(
+                        CupertinoModalSheetRoute<dynamic>(
+                          builder: buildSheet,
+                        ),
                       );
                     },
                   ),
-                ),
-              );
-            }),
-          ),
+                  CupertinoButton(
+                    child: const Text('Go to Home'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        Widget buildHomePage(BuildContext context) {
+          return CupertinoPageScaffold(
+            key: const ValueKey('home-page'),
+            child: Center(
+              child: CupertinoButton(
+                child: const Text('Go to Detail'),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute<dynamic>(
+                      builder: buildDetailPage,
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        }
+
+        await tester.pumpWidget(
+          CupertinoApp(home: Builder(builder: buildHomePage)),
         );
+        expect(find.byId('home-page'), findsOneWidget);
 
-        // 1. Initial state: Home page
-        expect(find.byKey(homeToDetailButtonKey), findsOneWidget);
-
-        // 2. Navigate to Detail page
-        await tester.tap(find.byKey(homeToDetailButtonKey));
+        await tester.tap(find.text('Go to Detail'));
         await tester.pumpAndSettle();
-        expect(find.text('Detail Page'), findsOneWidget);
-        expect(find.byKey(detailShowSheetButtonKey), findsOneWidget);
-        expect(find.byKey(detailGoHomeButtonKey), findsOneWidget);
+        expect(find.byId('detail-page'), findsOneWidget);
 
-        // 3. Show Modal Sheet
-        await tester.tap(find.byKey(detailShowSheetButtonKey));
+        await tester.tap(find.text('Show Modal Sheet'));
         await tester.pumpAndSettle();
-        expect(find.byKey(sheetCloseButtonKey), findsOneWidget);
+        expect(find.byId('sheet'), findsOneWidget);
 
-        // 4. Close Modal Sheet
-        await tester.tap(find.byKey(sheetCloseButtonKey));
+        await tester.tap(find.text('Close Sheet'));
         await tester.pumpAndSettle();
-        // We should be back on the Detail page
-        expect(find.text('Detail Page'), findsOneWidget);
-        expect(find.byKey(sheetCloseButtonKey), findsNothing);
-        expect(find.byKey(detailShowSheetButtonKey), findsOneWidget);
+        expect(find.byId('detail-page'), findsOneWidget);
 
-        // 5. Go to Home from Detail page (Pop DetailPage)
-        // This is the step that was causing an exception.
-        await tester.tap(find.byKey(detailGoHomeButtonKey));
+        await tester.tap(find.text('Go to Home'));
+        await tester.pumpAndSettle();
         final errors = await tester.pumpAndSettleAndCaptureErrors();
-
-        // Assert no errors
-        expect(errors, isEmpty,
-            reason:
-                'Expected no errors after popping DetailPage, but got: $errors');
-        // Assert we are on the Home page
-        expect(find.byKey(homeToDetailButtonKey), findsOneWidget);
-        expect(find.text('Detail Page'), findsNothing);
+        expect(errors, isEmpty);
+        expect(find.byId('detail-page'), findsNothing);
       },
     );
   });
