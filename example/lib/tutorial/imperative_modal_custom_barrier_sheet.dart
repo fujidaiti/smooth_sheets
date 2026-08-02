906650e5ec5699db5e03c2e2ffbb1c6b607d3e38
fujidaiti <fujidaiti@gmail.com>
2026-08-02 18:23:24 +0900

    wip

--- example/lib/tutorial/imperative_modal_custom_barrier_sheet.dart
+++ example/lib/tutorial/imperative_modal_custom_barrier_sheet.dart
@@ -4,39 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:smooth_sheets/smooth_sheets.dart';
 
 void main() {
-  runApp(const _ImperativeModalSheetCustomBarrierExample());
+  runApp(const MaterialApp(home: _Home()));
 }
 
-class _ImperativeModalSheetCustomBarrierExample extends StatelessWidget {
-  const _ImperativeModalSheetCustomBarrierExample();
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: _ExampleHome());
-  }
-}
-
-class _ExampleHome extends StatefulWidget {
-  const _ExampleHome();
-
-  @override
-  State<_ExampleHome> createState() => _ExampleHomeState();
-}
-
-class _ExampleHomeState extends State<_ExampleHome> {
-  late final SheetController sheetController;
-
-  @override
-  void initState() {
-    super.initState();
-    sheetController = SheetController();
-  }
-
-  @override
-  void dispose() {
-    sheetController.dispose();
-    super.dispose();
-  }
+class _Home extends StatelessWidget {
+  const _Home();
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +16,7 @@ class _ExampleHomeState extends State<_ExampleHome> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          FlutterLogo(size: 200),
+          const FlutterLogo(size: 200),
           Center(
             child: ElevatedButton(
               onPressed: () => _showModalSheet(context),
@@ -57,34 +29,20 @@ class _ExampleHomeState extends State<_ExampleHome> {
   }
 
   void _showModalSheet(BuildContext context) {
-    const sheetHeight = 600.0;
-
     final modalRoute = ModalSheetRoute(
       swipeDismissible: true,
       barrierBuilder: (route, onDismissCallback) {
         return GestureDetector(
           onTap: onDismissCallback,
-          // AnimatedBuilder rebuilds when the sheet position changes
-          child: AnimatedBuilder(
-            animation: sheetController,
-            builder: (context, child) {
-              final offset = sheetController.value;
-
-              // SheetController.value returns pixels, not a 0-1 range
-              // Divide by sheet height to normalize
-              final pixelOffset = offset ?? 0.0;
-              final progress = (pixelOffset / sheetHeight).clamp(0.0, 1.0);
-
-              final blurAmount = progress * 15.0;
-
+          child: ValueListenableBuilder(
+            valueListenable: (route as ModalSheetRouteMixin).sheetVisibility,
+            builder: (context, visibility, child) {
+              final blur = visibility * 15.0;
               return BackdropFilter(
-                filter: ImageFilter.blur(
-                  sigmaX: blurAmount,
-                  sigmaY: blurAmount,
-                ),
+                filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.black.withValues(alpha: 0.3 * progress),
+                    color: Colors.black.withAlpha((0x84 * visibility).round()),
                   ),
                   child: const SizedBox.expand(),
                 ),
@@ -97,32 +55,22 @@ class _ExampleHomeState extends State<_ExampleHome> {
         minFlingVelocityRatio: 2.0,
         dismissalOffset: SheetOffset.proportionalToViewport(0.4),
       ),
-      builder: (context) =>
-          _ExampleSheet(controller: sheetController, height: sheetHeight),
+      builder: (context) => const _Sheet(),
     );
 
     Navigator.push(context, modalRoute);
   }
 }
 
-class _ExampleSheet extends StatelessWidget {
-  const _ExampleSheet({required this.controller, required this.height});
-
-  final SheetController controller;
-  final double height;
+class _Sheet extends StatelessWidget {
+  const _Sheet();
 
   @override
   Widget build(BuildContext context) {
     return Sheet(
-      controller: controller,
       snapGrid: const SheetSnapGrid(snaps: [SheetOffset(0.4), SheetOffset(1)]),
-      decoration: MaterialSheetDecoration(
-        size: SheetSize.fit,
-        borderRadius: BorderRadius.circular(20),
-        clipBehavior: Clip.antiAlias,
-      ),
       child: Container(
-        height: height,
+        height: 600,
         width: double.infinity,
         color: Theme.of(context).colorScheme.surface,
       ),
